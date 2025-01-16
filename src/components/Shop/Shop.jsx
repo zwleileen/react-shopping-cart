eb2b986e0f22441fca621d61f1e4e2aebdb239a1
eileen <zwleileen@gmail.com>
2025-01-16 14:55:01 +0800

    feat: completed assignment

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,28 +1,31 @@
 // src/components/Shop/Shop.jsx
-
+import './Shop.css'
 import { useState } from "react";
 import { inventoryData } from "../../data/data";
-import InventoryList from "../InventoryList";
+import InventoryList from '../InventoryList/InventoryList';
+
 
 const Shop = () => {
 
     const [shopInventory, setShopInventory] = useState(inventoryData);
     const [userInventory, setUserInventory] = useState([])
 
-    // const handleAddItem = () => {
-    //     setUserInventory([{
-    //         _id: "",
-    //         name: "",
-    //         price: "",
-    //     }])
-    // }
+    const handleAddItem = (item) => {
+        setUserInventory([...userInventory, item])
+        setShopInventory(shopInventory.filter((inv) => inv._id !== item._id))
+    }
+
+    const handleRemoveItem = (item) => {
+        setUserInventory(userInventory.filter((inv) => inv._id !== item._id))
+        setShopInventory([...shopInventory, item])
+    }
 
     return (
       <main>
         <h1>Shop</h1>
-        <section>
-            <InventoryList title="Shop Inventory" inventory={shopInventory}/>
-            <InventoryList title="User Inventory" inventory={userInventory}/>
+        <section className="shop-section">
+            <InventoryList title="Shop Inventory" inventory={shopInventory} handleAddItem={handleAddItem}/>
+            <InventoryList title="User Inventory" inventory={userInventory} handleRemoveItem={handleRemoveItem}/>
         </section>
       </main>
     );
